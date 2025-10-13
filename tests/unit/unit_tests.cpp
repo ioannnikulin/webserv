2a5754c1112aaeca3e12b00766fe2027c6805cf2
ioann <ioann.nikulin@gmail.com>
2025-10-13 19:39:45 +0200

    tests with plain asserts

--- tests/unit/unit_tests.cpp
+++ tests/unit/unit_tests.cpp
@@ -1,10 +1,12 @@
 #include "tests.hpp"
 
-int unit_tests() {
-    std::string request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
-    std::string expected_response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-    std::string actual_response = generate_response(request);
+using std::string;
 
-    assert(actual_response == expected_response);
+int unit_tests() {
+    string request = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+    string expected_response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+    string actual_response = generate_response(request);
+
+    assert(expected_response == actual_response);
     return (0);
 }
