c6edac9fb1407b0e0fa7b01806070d0af66e4914
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2025-10-30 17:14:56 +0100

    testing cland-tidy fail in github, adding comments to makefile

--- sources/response_generator/response_generator.cpp
+++ sources/response_generator/response_generator.cpp
@@ -7,5 +7,7 @@ using std::string;
 // TODO: issue 8
 string generateResponse(const string& request) {
     (void)request;
-    return ("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
+    int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20;
+    a1 = a2 = a3 = a4 = a5 = a6 = a7 = a8 = a9 = a10 = a11 = a12 = a13 = a14 = a15 = a16 = a17 = a18 = a19 = a20 = 0;
+    return ("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 }
