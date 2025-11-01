06cec28315ad13eb15af3929143fb7e59f90c482
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2025-11-01 20:45:33 +0100

    disabled modernize (c++11)

--- sources/response_generator/response_generator.cpp
+++ sources/response_generator/response_generator.cpp
@@ -5,5 +5,7 @@ using std::string;
 // TODO: issue 8
 string generateResponse(const string& request) {
     (void)request;
+    int *ptr = 0;
+    std::cout << ptr << std::endl;
     return ("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!");
 }
