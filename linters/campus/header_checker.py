18d1b987ec7c08b47b8c1c2422e3de69eddd6358
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2025-11-22 19:10:25 +0100

    no duplication in makefile, custom checkers now exist in campus version and full version, selected automatically

---
+++ linters/campus/header_checker.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from pathlib import Path
+
+roots = sys.argv[1:] if len(sys.argv) > 1 else ["sources", "include", "tests"]
+headerSuffixes = {".h", ".hpp", ".hh", ".hxx"}
+
+regexpUsing = re.compile(r"^\s*using\s", re.M)
+regexpCommentOneLine = re.compile(r'//(.*)')
+regexpCommentMultiline = re.compile(r'/\*(.*?)\*/', re.DOTALL)
+regexpCommentValidContentPrefix = re.compile(r'^(( TODO [0-9]+:)|( NOTE: )|( namespace))')
+
+def collectFiles(roots):
+    files = []
+    for r in roots:
+        p = Path(r)
+        if p.is_file() and p.suffix in headerSuffixes:
+            files.append(p)
+        elif p.is_dir():
+            files += [f for f in p.rglob("*") if f.suffix in headerSuffixes]
+    return (sorted(files))
+
+def checkCommentPrefixes(s, issues):
+    for match in regexpCommentOneLine.finditer(s):
+        comment = match.group(1)
+        if not regexpCommentValidContentPrefix.match(comment):
+            issues.append("contains an unclassified oneline comment, please mark as TODO or NOTE")
+            break
+    for match in regexpCommentMultiline.finditer(s):
+        comment = match.group(1)
+        if not regexpCommentValidContentPrefix.match(comment):
+            issues.append("contains an unclassified multiline comment, please mark as TODO or NOTE")
+            break
+
+def textChecks(f):
+    s = f.read_text()
+    issues = []
+    if regexpUsing.search(s):
+        issues.append("contains 'using' directive (forbidden in headers)")
+    checkCommentPrefixes(s, issues)
+    return issues
+
+def main():
+    print("Running header-checker...")
+    files = collectFiles(roots)
+    if not files:
+        print("No header files found.")
+        return
+
+    failed = False
+    for f in files:
+        issues = textChecks(f)
+        if issues:
+            failed = True
+            print(f"{f}: " + "\n\t".join(issues))
+    if failed:
+        sys.exit(1)
+    else:
+        print("header-checker found no issues")
+        print("WARNING!")
+        print("This is a limited header-checker with only whatever checks are available in campus")
+        print("A full header-checker can be more picky")
+
+if __name__ == "__main__":
+    main()
---
+++ linters/campus/source_checker.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from pathlib import Path
+from header_checker import checkCommentPrefixes
+
+roots = sys.argv[1:] if len(sys.argv) > 1 else ["sources", "include", "tests"]
+headerSuffixes = {".cpp", ".c"}
+
+def collectFiles(roots):
+    files = []
+    for r in roots:
+        p = Path(r)
+        if p.is_file() and p.suffix in headerSuffixes:
+            files.append(p)
+        elif p.is_dir():
+            files += [f for f in p.rglob("*") if f.suffix in headerSuffixes]
+    return (sorted(files))
+
+regexpUsingNamespace = re.compile(r"^\s*using\snamespace\s", re.M)
+regexpBad_ReturnNoSpaceOrParens = re.compile(
+    # fail for `return;` 	(first part; only space after return),
+	# `return(` 			(same),
+	# `return x` 			(second part: after a space only `;` or `)` allowed).
+    # allowed: `return ;` `return (x);`
+    r"^\s*return(([^ ])|( [^\\(;]))", re.M
+)
+regexpPoll = re.compile(f"^bpoll\\(")
+
+files = collectFiles(roots)
+if not files:
+    print("No source files found.")
+    sys.exit(0)
+
+failed = False
+pollCalls = 0
+
+for f in files:
+    s = f.read_text()
+
+    issues = []
+
+    if regexpBad_ReturnNoSpaceOrParens.search(s):
+        issues.append("please wrap returned values in parenthesis, preceeded by a space")
+
+    if regexpUsingNamespace.search(s):
+        issues.append("contains 'using namespace' directive; please switch to explicit directive like 'using std::string' etc.")
+
+    checkCommentPrefixes(s, issues)
+
+    if regexpPoll.search(s):
+        pollCalls += 1
+
+    if issues:
+        failed = True
+        print(f"{f}: " + "\n\t".join(issues))
+
+if pollCalls > 1:
+    failed = True
+    print("too many poll() calls detected")
+
+if failed:
+    sys.exit(1)
+else:
+    print("source checker didn't find any issues")
