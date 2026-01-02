ddaf9b68aeb12f884e73cd482f383de017900780
shapkina-l <shapkina199@gmail.com>
2026-01-03 00:33:45 +0100

    http statuses squashed

--- sources/http_status/HttpStatus.hpp
+++ sources/http_status/HttpStatus.hpp
@@ -60,6 +60,7 @@ public:
         INTERNAL_SERVER_ERROR = 500,
         NOT_IMPLEMENTED = 501,
         BAD_GATEWAY = 502,
+        HTTP_SERVICE_UNAVAILABLE = 503,
         HTTP_VERSION_NOT_SUPPORTED = 505
     };
 };
