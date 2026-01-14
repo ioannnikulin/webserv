296187ddbc9480fe22af341a3e7571173aaa82b5
Dima MBP M2 Parallels <dmitrijs.lasko@gmail.com>
2026-01-14 01:12:43 +0100

    main message to FATAL; def level to INFO

--- sources/webserv.cpp
+++ sources/webserv.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
 #include <string>
 
 #include "WebServer.hpp"
-#include "utils/colors.hpp"
+#include "logger/Logger.hpp"
 
 int main(int argc, char* argv[]) {
+    webserver::Logger _log;
     if (argc != 2) {
-        std::cout << B_RED << "Failed to launch: no config file provided. Usage: " << argv[0]
-                  << " <config_file>\n"
-                  << RESET_COLOR << "Example: ./webserv tests/config_files/local_run.conf\n";
+        _log.stream(LOG_FATAL) << "Failed to launch: no config file provided.\n"
+                               << "Usage:   " << argv[0] << " <config_file>\n"
+                               << "Example: " << argv[0]
+                               << " ./tests/config_files/local_run.conf\n";
         return (1);
     }
     webserver::WebServer& server = webserver::WebServer::getInstance(argv[1]);
