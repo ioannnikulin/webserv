9c736f54e205bcf8a55ea3586dbc80ad7703a4cd
Dima MBP M2 Parallels <dmitrijs.lasko@gmail.com>
2026-01-14 01:12:43 +0100

    main message to FATAL; def level to INFO

--- sources/logger/LoggerConfig.hpp
+++ sources/logger/LoggerConfig.hpp
@@ -2,7 +2,7 @@
 #define LOGGERCONFIG_HPP
 
 // NOTE: DL: I wanted to move all settings to a separate setting file but doing this through macros is seemingly a bad idea
-#define DEFAULT_LOG_LEVEL LOG_TRACE
+#define DEFAULT_LOG_LEVEL LOG_INFO
 #define GLOBAL_LOG_LEVEL LOG_INFO
 
 #define C_RESET RESET_COLOR
